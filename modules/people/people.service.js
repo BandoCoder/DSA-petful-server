06c1e397e7761538f00c0b16f629966e2c81dc3e
kittybagpipes <sparticus978@gmail.com>
2021-01-22 09:56:13 -0800

    built services

--- modules/people/people.service.js
+++ modules/people/people.service.js
@@ -1,24 +1,29 @@
-const Queue = require('../queue/Queue')
-const store = require('../../store')
+const Queue = require("../queue/Queue");
+const store = require("../../store");
 
 // Set up initial data.
 // --------------------
 
-const people = new Queue()
-store.people.forEach(person => people.enqueue(person))
+const people = new Queue();
+store.people.forEach((person) => people.enqueue(person));
 
 // --------------------
 
 module.exports = {
   get() {
     // Return all people in the queue.
+    let input = people.all();
+    const peopleArr = input.reverse();
+    return peopleArr;
   },
 
   enqueue(person) {
     // Add a person to the queue.
+    return people.enqueue(person);
   },
 
   dequeue() {
     // Remove a person from the queue.
-  }
-}
+    return people.dequeue();
+  },
+};
--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -1,25 +1,40 @@
-const Queue = require('../queue/Queue')
-const store = require('../../store')
+const Queue = require("../queue/Queue");
+const store = require("../../store");
 
 // Set up initial data.
 // --------------------
 
 const pets = {
   cats: new Queue(),
-  dogs: new Queue()
-}
+  dogs: new Queue(),
+};
 
-store.cats.forEach(cat => pets.cats.enqueue(cat))
-store.dogs.forEach(dog => pets.dogs.enqueue(dog))
+store.cats.forEach((cat) => pets.cats.enqueue(cat));
+store.dogs.forEach((dog) => pets.dogs.enqueue(dog));
 
 // --------------------
 
 module.exports = {
   get() {
     // Return the pets next in line to be adopted.
+    let inputCats = pets.cats.all();
+    let inputDogs = pets.dogs.all();
+
+    const petObj = {
+      cats: inputCats.reverse(),
+      dogs: inputDogs.reverse(),
+    };
+    return petObj;
   },
 
   dequeue(type) {
+    // Stringify the type
     // Remove a pet from the queue.
-  }
-}
+    if (type === "cats") {
+      return pets.cats.dequeue();
+    }
+    if (type === "dogs") {
+      return pets.dogs.dequeue();
+    }
+  },
+};
